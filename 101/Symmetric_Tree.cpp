470feebc64df1cabf4f07ff571c26687711dfdef
Mini_Spinach <526353363@qq.com>
2020-06-10 10:10:31 +0800

    Create  Symmetric_Tree.cpp

---
+++ 101/Symmetric_Tree.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* left, TreeNode* right) {
+        if (left && right) {
+            return (left -> val == right -> val)
+                   && check(left -> left, right -> right)
+                   && check(left -> right, right -> left);
+        }
+        return (!left && !right) ? true : false;
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        return root ? check(root -> left, root -> right) : true;
+    }
+};
