211f41875935a8567a9ec3decf95bd6a364ee205
Mini_Spinach <526353363@qq.com>
2020-06-14 17:43:15 +0800

    Update Best _Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp

--- 309/Best _Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
+++ 309/Best _Time_to_Buy_and_Sell_Stock_with_Cooldown.cpp
@@ -17,7 +17,7 @@ public:
         for (tp i = (tp)1; i < sz; ++i) {
             buy[i] = std::max(buy[i-1], cool[i - 1] - prices[i]);    // 之前的最后一次操作是买，可以cool，是cool的话可以继续买
             sell[i] = std::max(buy[i-1] + prices[i], sell[i - 1]);   // 之前的最后一次操作是sell，可以cool，是buy的话可以直接卖
-            cool[i] = std::max(std::max(cool[i - 1], sell[ i - 1]), buy[i - 1]);  // 什么情况下都可以无条件cool
+            cool[i] = sell[ i - 1]);  // 什么情况下都可以无条件cool
         }
         return sell[sz - 1];
     }
