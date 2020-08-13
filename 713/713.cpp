14b51ff27b381332df391fe81c6172d546f166dc
Mini_Spinach <526353363@qq.com>
2020-08-13 11:22:38 +0800

    Update 713.cpp

--- 713/713.cpp
+++ 713/713.cpp
@@ -1,30 +1,22 @@
 #include <deque>
 
-#不会溢出的版本，需要用空间换取
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int32_t res = 0;
-		std::deque<int> temp;
-		for (const auto& num : nums) {
-			if (num >= k) {
-				temp.clear();
-			}
-            else if (num == 1) {
-				res += temp.size() + 1;
-				temp.push_back(num);
-			}
-			else {
-				while (temp.size() && temp.front() * num >= k){
-					temp.pop_front();
-				}
-				for (auto it = temp.begin(); it!= temp.end(); ++it) {
-					*it *= num;
-				}
-				res += temp.size() + 1;
-				temp.push_back(num);
-			}
-		}
-		return res;
+        int count = 0;
+        int begin = 0;
+        int end = 0;
+        int sum = 1;
+        while(end < nums.size()) {
+            sum *= nums[end];
+            while (sum >= k && begin <= end) {
+                sum /= nums[begin];
+                begin++;
+            }
+            count += end - begin + 1;
+            end++;
+        }
+
+        return count;
     }
 };
